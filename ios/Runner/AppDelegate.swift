2d0a6468770b9c7cbf999ad0e94fb16815568958
PradipatEart <PradiEart118@gmail.com>
2026-03-09 14:33:43 +0700

    Temporary remove Notificaion imports

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import flutter_local_notifications
+// import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,13 +8,13 @@ import flutter_local_notifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-      GeneratedPluginRegistrant.register(with: registry)
-    }
+    // FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    //   GeneratedPluginRegistrant.register(with: registry)
+    // }
     GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
+    // if #available(iOS 10.0, *) {
+    //   UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    // }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/setting.dart
+++ lib/setting.dart
@@ -1,14 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dog_syndrome/services/dog_service.dart';
+import 'package:dog_syndrome/services/user_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SettingPage extends StatelessWidget {
-  const SettingPage({super.key});
+UserFirestoreService userFirestoreService = UserFirestoreService();
+
+class SettingPage extends StatefulWidget {
+  SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+
+  String? uid = FirebaseAuth.instance.currentUser?.uid;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Setting')),
-      body: Container(
-        color: Colors.red,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFFD47E30),
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset('assets/images/background_fade.png'),
+            StreamBuilder<DocumentSnapshot>(
+              stream: userFirestoreService.getUserStream(uid!),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Something went wrong!");
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column();
+                }
+                if (!snapshot.hasData || !snapshot.data!.exists) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) async {
+                    final user = FirebaseAuth.instance.currentUser;
+    
+                    try {
+                      await user?.delete();
+                      debugPrint("Auth Account Deleted Successfully");
+                    } catch (e) {
+                      debugPrint("Auth Deletion failed: $e");
+                    }
+
+                    await FirebaseAuth.instance.signOut();
+                    
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Account not found. Please try again."),
+                        backgroundColor: Colors.redAccent,
+                      ),
+                    );
+                    Navigator.pushNamedAndRemoveUntil(context, '/authen', (route) => false);
+                  });
+                  return const Center(child: CircularProgressIndicator()); 
+                }
+
+                var userData = snapshot.data!.data() as Map<String, dynamic>;
+
+                return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 80,),
+                      
+                    ]
+                  );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
